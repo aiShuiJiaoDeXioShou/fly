b9ce0bd216e812b378c425abba2b4c7e675a1ef1
aiShuiJiaoDeXioShou <2832294398@qq.com>
2022-06-30 12:34:26 +0800

    假装我写了代码

---
+++ fly_desktop/src/components/card/BoxCard.vue
@@ -0,0 +1,29 @@
+<!-- 用于展示首页菜单 -->
+<template>
+    <div class="card-root">
+        你好世界
+    </div>
+</template>
+
+<script>
+export default {
+    props:["card"],
+    data() {
+        return {
+        }
+    }
+}
+
+</script>
+
+<style lang='less' scoped>
+.card-root {
+    max-width: 30vw;
+    min-width: 275px;
+    height: 30vh;
+    box-shadow: 0 15px 25px rgb(0, 0, 0, 0.1);
+    backdrop-filter: blur(30px);
+    margin-right: 1vw;
+    margin-top: 1vw;
+}
+</style>
---
+++ fly_desktop/src/views/home/Index.vue
@@ -0,0 +1,54 @@
+<!-- 首页显示菜单 -->
+<template>
+  <div class="index-root">
+
+    <div class="index-data">
+      <div v-for="(item, index) in 3" :key="index">
+        <box-card :card="card"></box-card>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import BoxCard from '../../components/card/BoxCard.vue'
+export default {
+  name: 'Index',
+  data() {
+    return {
+      card: {}
+    }
+  },
+  components: {
+    BoxCard,
+  }
+}
+
+</script>
+
+<style lang='less' scoped>
+.index-root {
+
+  .index-data {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+  }
+}
+
+// 当当前页面大小大于等于1024px时，设置为100vw，否则设置为30vw
+@media (min-width: 580px) {
+
+  .index-root {
+    // 子元素垂直居中
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+}
+</style>
