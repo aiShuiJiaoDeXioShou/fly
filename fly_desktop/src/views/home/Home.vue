986dacf1e4f352d43b03bccc6f01a50b154bf273
aiShuiJiaoDeXioShou <2832294398@qq.com>
2022-07-10 01:29:29 +0800

    修改了测试页面，测试页面变得更加好看了

--- fly_desktop/src/views/home/Home.vue
+++ fly_desktop/src/views/home/Home.vue
@@ -2,39 +2,99 @@
   <div class="home">
     <aside class="show-menu menu">
       <ul class="menu_list menu-list">
-        <li>
-          <router-link class="menu-link" to="/home/">起点月票榜</router-link>
+        <li class="menu_item">
+          <div class="menu_icon">
+            <img src="../../assets/img/home_icon/duxiaoshuo.png" alt="">
+          </div>
+          <div class="menu_kp_link">
+            <router-link class="menu-link" to="/home/">起点月票榜</router-link>
+          </div>
         </li>
-        <li>
-          <router-link class="menu-link" to="/note_edit">文本编辑器</router-link>
+
+        <li class="menu_item">
+          <div class="menu_icon">
+            <img src="../../assets/img/home_icon/duxiaoshuo.png" alt="文本编辑器">
+          </div>
+          <div class="menu_kp_link">
+            <router-link class="menu-link" to="/note_edit">文本编辑器</router-link>
+          </div>
         </li>
-        <li>
-          <router-link class="menu-link" to="/read_write">读写操作</router-link>
+
+        <li class="menu_item">
+          <div class="menu_icon">
+            <img src="../../assets/img/home_icon/duxiaoshuo.png" alt="读写">
+          </div>
+          <div class="menu_kp_link">
+            <router-link class="menu-link" to="/read_write">读写操作</router-link>
+          </div>
         </li>
+
         <li>
-          <p>分类功能测试用例</p>
+          <h2>测试</h2>
           <ul>
-            <li>
-              <router-link class="menu-link" to="/class_box">分类盒子</router-link>
+            <li class="menu_item">
+              <div class="menu_icon">
+                <img src="../../assets/img/home_icon/box.png" alt="盒子">
+              </div>
+              <div class="menu_kp_link">
+                <router-link class="menu-link" to="/class_box">分类盒子</router-link>
+              </div>
             </li>
-            <li>
-              <router-link class="menu-link" to="/class_stl">分类模板</router-link>
+
+            <li class="menu_item">
+              <div class="menu_icon">
+                <img src="../../assets/img/home_icon/mobankuangjia.png" alt="模板">
+              </div>
+              <div class="menu_kp_link">
+                <router-link class="menu-link" to="/class_stl">分类模板</router-link>
+              </div>
             </li>
-            <li>
-              <router-link class="menu-link" to="/class_export">分类导出</router-link>
+
+            <li class="menu_item">
+              <div class="menu_icon">
+                <img src="../../assets/img/home_icon/daochu.png" alt="分类导出">
+              </div>
+              <div class="menu_kp_link">
+                <router-link class="menu-link" to="/class_export">分类导出</router-link>
+              </div>
             </li>
-            <li>
-              <router-link class="menu-link" to="/class_import">分类导入</router-link>
+
+            <li class="menu_item">
+              <div class="menu_icon">
+                <img src="../../assets/img/home_icon/daoru.png" alt="分类导入">
+              </div>
+              <div class="menu_kp_link">
+                <router-link class="menu-link" to="/class_import">分类导入</router-link>
+              </div>
             </li>
-            <li>
-              <router-link class="menu-link" to="/note_edit">日记功能</router-link>
+
+            <li class="menu_item">
+              <div class="menu_icon">
+                <img src="../../assets/img/home_icon/duxiaoshuo.png" alt="日记功能">
+              </div>
+              <div class="menu_kp_link">
+                <router-link class="menu-link" to="/note_edit">日记功能</router-link>
+              </div>
             </li>
-            <li>
-              <router-link class="menu-link" to="/qq_assistant">qq群助手</router-link>
+
+            <li class="menu_item">
+              <div class="menu_icon">
+                <img src="../../assets/img/home_icon/QQ.png" alt="qq群助手">
+              </div>
+              <div class="menu_kp_link">
+                <router-link class="menu-link" to="/qq_assistant">qq群助手</router-link>
+              </div>
             </li>
-            <li>
-              <router-link class="menu-link" to="/mimicry_menu">拟态UI</router-link>
+
+            <li class="menu_item">
+              <div class="menu_icon">
+                <img src="../../assets/img/home_icon/bianjiqi.png" alt="拟态UI">
+              </div>
+              <div class="menu_kp_link">
+                <router-link class="menu-link" to="/mimicry_menu">拟态UI</router-link>
+              </div>
             </li>
+
           </ul>
         </li>
       </ul>
@@ -46,24 +106,26 @@
 </template>
 
 <script>
+import $ from 'jquery'
 
 export default {
   name: 'Home',
   methods: {
     init() {
       // 获取所有class为menu-获取所有class为menu-link的元素
-      let menu_links = document.querySelectorAll('.menu_list .menu-link')
-      menu_links[0].classList.add('is-active')
+      let menu_links = $('.menu_list .menu-link')
+      menu_links.eq(0).addClass('is-active')
+      $('.menu_icon').eq(0).addClass('menu_icon_select')
       // 遍历所有menu-link元素
-      menu_links.forEach(item => {
-        item.addEventListener('click', () => {
-          // 移除所有class为menu-link的元素的class为active
-          let active = document.querySelector('.menu-link.is-active')
-          if (active)
-            active.classList.remove('is-active')
-          // 给当前点击的元素添加class为active
-          item.classList.add('is-active')
-        })
+      menu_links.click(function () {
+        // 移除所有class为menu-link的元素的class为active
+        let item_link = $(this).parent().prev()
+        $('.menu-link.is-active').removeClass('is-active')
+        $('.menu_icon').removeClass('menu_icon_select')
+        // 给当前点击的元素添加class为active
+        this.classList.add('is-active')
+        // 找到该元素父亲的上一个兄弟
+        item_link.addClass('menu_icon_select')
       })
     }
   },
@@ -79,14 +141,15 @@ export default {
 <style lang="less" scoped>
 .home {
   display: flex;
+  min-height: 100vh;
   height: 100%;
   padding-top: @Menu_H * 1.5;
   position: relative;
 
 
   .show-menu {
-    flex: 0.2;
     padding: 0 10px;
+    flex: 0.2;
   }
 
   .show {
@@ -99,6 +162,45 @@ export default {
     // 防止换行
     white-space: nowrap;
 
+    .menu_item {
+      display: flex;
+
+      .menu_icon {
+
+        padding: 0px 5px 0px 5px;
+        border-radius: 4px;
+        line-height: 40px;
+        margin-right: 10px;
+        cursor: pointer;
+
+        img {
+          width: 20px;
+          height: auto;
+        }
+
+        &:hover {
+          background-color: rgba(0, 0, 0, 0.2);
+        }
+
+      }
+
+      // 选中menu_icon时触发的标签
+      .menu_icon_select {
+        border-left: 2px solid #91d5ff;
+        background-color: rgba(0, 0, 0, 0.2);
+      }
+
+      .menu_kp_link {
+        flex: 0.8;
+      }
+    }
+  }
+
+  // 当界面小于某个值时，菜单收起来
+  @media (max-width: 896px) {
+    .menu_kp_link {
+      display: none;
+    }
   }
 }
 </style>
